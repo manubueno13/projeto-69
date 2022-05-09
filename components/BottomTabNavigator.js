36c9c2380f1d58c1c4cdc47f13df6f0b4046ab8c
Thiago Saraiva <saraii_@live.com>
2022-05-09 17:15:05 -0300

    Add files via upload

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import RideScreen from "../screens/Ride";
 import RideHistoryScreen from "../screens/RideHistory";
@@ -10,12 +11,14 @@ const Tab = createBottomTabNavigator();
 export default class BottomTabNavigator extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Alugue uma Bicicleta" component={RideScreen} />
-          <Tab.Screen name="Histórico de Locações" component={RideHistoryScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Alugue uma Bicicleta" component={RideScreen} />
+            <Tab.Screen name="Histórico de Locações" component={RideHistoryScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     );
   }
 }
